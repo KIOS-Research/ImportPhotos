7e4d778facbb8242fcba7ca11024d0938c2dc9b7
Marios Kyriakou <mariosmsk@gmail.com>
2019-03-07 19:03:50 +0200

    Update MouseClick.py

--- MouseClick.py
+++ MouseClick.py
@@ -66,10 +66,7 @@ class MouseClick(QgsMapTool):
             fields = [field.name().upper() for field in layer.fields()]
             if 'PATH' or 'PHOTO' in fields:
                 lRect = self.canvas.mapSettings().mapToLayerCoordinates(layer, rect)
-                try:
-                    layer.selectByRect(lRect, False)
-                except:
-                    layer.select(lRect, False)
+                layer.selectByRect(lRect, False)
                 selected_features = layer.selectedFeatures()
                 if selected_features != []:
                     layersSelected.append(layer)
