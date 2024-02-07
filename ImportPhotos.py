cdca50069ff08cd248df453092fa40a3c8dcad84
Marios S. Kyriakou <mkiria01@ucy.ac.cy>
2024-02-07 10:11:14 +0200

    Update - based on the pull #110

--- ImportPhotos.py
+++ ImportPhotos.py
@@ -548,7 +548,7 @@ class ImportPhotos:
             pass
 
         pictures_to_remove = list(set(picture_paths) - set(list_pictures))
-        pictures_to_add = list(set(list_pictures) - set(picture_paths)).sorted()
+        pictures_to_add = sorted(list(set(list_pictures) - set(picture_paths)))
 
         editing_started = self.selected_layer.startEditing()
         if editing_started:
